4233b25a1aed750424d2904379047fc60fc41317
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-07-21 14:02:15 -0500

    constructor

---
+++ index.js
@@ -0,0 +1,7 @@
+class Timer {
+    constructor(durationInput, startButton, pauseButton ) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+    }
+}
